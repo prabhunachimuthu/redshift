3dd4f6044d6b8e41a4d9c2d1fca32b191045e491
prabhu <heroku@softtrends.com>
2021-04-13 08:42:53 +0530

    changes

--- .profile.d/configure-odbc.sh
+++ .profile.d/configure-odbc.sh
@@ -5,9 +5,13 @@ export ODBCSYSINI=${HOME}/.apt/usr/lib/odbc/conf/
 
 mkdir -p /etc
 
-echo "[redshiftodbc]
+echo "[redshift]
+Description=RedshiftDB
 Driver=Amazon Redshift (x64)
-Description=Amazon Redshift (x64)
+Locale=en-US
+PORT=5439
+SSL=on
+CLIENT_SESSION_KEEP_ALIVE=true
 uid=${REDSHIFT_USERNAME}
 pwd=${REDSHIFT_PASSWORD}
 QuickBaseServer=${REDSHIFT_SERVER}
